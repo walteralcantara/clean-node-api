64cfaccfc64791319fc76fefd05b2e43f30ea17a
Walter Alcantara <waltermalcantara@gmail.com>
2023-10-25 17:46:16 -0300

    feat: ensure SignUpController return an error if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,19 @@
 export class SignUpController {
   handle(httpRequest: any): any {
     httpRequest;
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name'),
-    };
+
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name'),
+      };
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email'),
+      };
+    }
   }
 }
