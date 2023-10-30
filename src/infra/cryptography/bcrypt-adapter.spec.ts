585ee4879b4c42e7cfb31d359b019ab3b6c2acfd
Walter Alcantara <waltermalcantara@gmail.com>
2023-10-30 09:11:26 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,14 @@
 import bcrypt from 'bcrypt';
 import { BcryptAdapter } from './bcrypt-adapter';
 
+jest.mock('bcrypt', () => {
+  return {
+    async hash(): Promise<string> {
+      return new Promise((resolve) => resolve('hash'));
+    },
+  };
+});
+
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
     const SALT = 12;
@@ -8,5 +16,12 @@ describe('Bcrypt Adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.encrypt('any_value');
     expect(hashSpy).toHaveBeenCalledWith('any_value', SALT);
+  });
+
+  test('Should return a hash on success', async () => {
+    const SALT = 12;
+    const sut = new BcryptAdapter(SALT);
+    const hash = await sut.encrypt('any_value');
+    expect(hash).toBe('hash');
   });
 });
