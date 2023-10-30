97ba8d390d1394a08ee0d9400ab15ac9222a4c99
Walter Alcantara <waltermalcantara@gmail.com>
2023-10-30 09:08:12 -0300

    test: ensure BcryptAdapter calls bcrypt with correct values

---
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,12 @@
+import bcrypt from 'bcrypt';
+import { BcryptAdapter } from './bcrypt-adapter';
+
+describe('Bcrypt Adapter', () => {
+  test('Should call bcrypt with correct values', async () => {
+    const SALT = 12;
+    const sut = new BcryptAdapter(SALT);
+    const hashSpy = jest.spyOn(bcrypt, 'hash');
+    await sut.encrypt('any_value');
+    expect(hashSpy).toHaveBeenCalledWith('any_value', SALT);
+  });
+});
