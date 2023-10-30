d4a1754cf128b6c0c97d4e85e0e2976a0643aa9e
Walter Alcantara <waltermalcantara@gmail.com>
2023-10-30 09:12:33 -0300

    feat: move sut creation to a factory helper method

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -9,18 +9,21 @@ jest.mock('bcrypt', () => {
   };
 });
 
+const SALT = 12;
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(SALT);
+};
+
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
-    const SALT = 12;
-    const sut = new BcryptAdapter(SALT);
+    const sut = makeSut();
     const hashSpy = jest.spyOn(bcrypt, 'hash');
     await sut.encrypt('any_value');
     expect(hashSpy).toHaveBeenCalledWith('any_value', SALT);
   });
 
   test('Should return a hash on success', async () => {
-    const SALT = 12;
-    const sut = new BcryptAdapter(SALT);
+    const sut = makeSut();
     const hash = await sut.encrypt('any_value');
     expect(hash).toBe('hash');
   });
